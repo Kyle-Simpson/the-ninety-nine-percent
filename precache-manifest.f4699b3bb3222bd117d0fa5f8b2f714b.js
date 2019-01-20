9719a285c79d8d1ead159029c0f7c9b691658aaa
Kyle Simpson <kes97@uw.edu>
2019-01-20 14:10:18 -0800

    Updates

---
+++ precache-manifest.f4699b3bb3222bd117d0fa5f8b2f714b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c601866820ca3ce50d4",
+    "url": "/kes97/journal/ninety-nine-percent/static/js/runtime~main.8c601866.js"
+  },
+  {
+    "revision": "71f40baa45a8d4f7b3dc",
+    "url": "/kes97/journal/ninety-nine-percent/static/js/main.71f40baa.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/kes97/journal/ninety-nine-percent/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "71f40baa45a8d4f7b3dc",
+    "url": "/kes97/journal/ninety-nine-percent/static/css/main.85639c3b.chunk.css"
+  },
+  {
+    "revision": "2e3f40720014ca6c147a65fe86c29960",
+    "url": "/kes97/journal/ninety-nine-percent/index.html"
+  }
+];
